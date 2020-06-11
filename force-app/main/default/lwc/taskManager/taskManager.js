eabd09fbacc7adb98b2cc08ba34a13e0d6c00542
caleb <calebplant@aggienetwork.com>
2020-06-11 14:21:20 -0500

    Component now pulls data from database

--- force-app/main/default/lwc/taskManager/taskManager.js
+++ force-app/main/default/lwc/taskManager/taskManager.js
@@ -1,11 +1,29 @@
 import { LightningElement, track } from 'lwc';
+import getUserToDoLists from '@salesforce/apex/TaskManagerController.getUserToDoLists';
 
 export default class TaskManager extends LightningElement {
     @track taskLists = [];
 
     connectedCallback()
     {
-        this.populateDummyData();
+        // this.populateDummyData();
+        this.fetchUserTodoLists();
+    }
+
+    fetchUserTodoLists()
+    {
+        getUserToDoLists({source: 'placeholder'}).then(result => {
+            if(result)
+            {
+                console.log("Size retrieved from server: ", result.length);
+                console.log(result);
+                
+                this.taskLists = result;
+            }
+        }).catch(error => {
+            console.log("Error fetching user todo lists!");
+            console.log(error);
+        });
     }
 
     populateDummyData()
