caf4e3bbfd4165b3b751841913114fc67be25a2f
caleb <calebplant@aggienetwork.com>
2020-06-11 21:33:05 -0500

    Added function to change Todo Task parent

--- force-app/main/default/lwc/taskManager/taskManager.js
+++ force-app/main/default/lwc/taskManager/taskManager.js
@@ -1,5 +1,6 @@
 import { LightningElement, track } from 'lwc';
 import getUserToDoLists from '@salesforce/apex/TaskManagerController.getUserToDoLists';
+import moveTodoTask from '@salesforce/apex/TaskManagerController.moveTodoTask';
 
 export default class TaskManager extends LightningElement {
     @track taskLists = [];
@@ -9,11 +10,26 @@ export default class TaskManager extends LightningElement {
         this.fetchUserTodoLists();
     }
 
+    // handleMoveTask(event)
+    // {
+    //     console.log('Caught in taskManager');
+    //     console.log('Event detail: ' + event.detail);
+        
+    // }
+    
     handleMoveTask(event)
     {
         console.log('Caught in taskManager');
         console.log('Event detail: ' + event.detail);
-        
+
+        const targetListId = 'a000R00000341ttQAA';
+        moveTodoTask({taskId: event.detail, targetListId: targetListId}).then(result => {
+            console.log('Successfully moved task.');
+            console.log(result);
+        }).catch(error => {
+            console.log('Error moving task!');
+            console.log(error);
+        });
     }
 
     fetchUserTodoLists()
